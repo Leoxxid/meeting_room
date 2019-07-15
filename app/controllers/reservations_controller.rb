cf13b143092826cac9dcc874739eb7a12ce9bfb0
leoxxid <leoxxid@hotmail.com>
2019-07-15 15:23:56 -0300

    Init controller reservations to manage reservation of meeting room

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,11 @@
+class ReservationsController < ApplicationController
+  def index; end
+  
+  def show; end
+
+  def create; end
+
+  def update; end
+
+  def destroy; end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_10_200546) do
+ActiveRecord::Schema.define(version: 2019_07_14_213722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reservations", force: :cascade do |t|
+    t.datetime "event_date"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
